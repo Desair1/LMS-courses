807b0babcd55396c8fbe5482f1574fa9be73f0c2
Desair1 <ilya_velichko_1995@mail.ru>
2025-06-02 20:24:40 +0300

    feat: Added hiding/revealing lesson details. refactor: delted unused route

--- src/entities/lesson/Lesson.tsx
+++ src/entities/lesson/Lesson.tsx
@@ -1,14 +1,36 @@
+import { useDispatch, useSelector } from "react-redux";
 import type ILesson from "../../types/lesson";
 import styles from "./lesson.module.scss";
+import type { AppDispatch, RootState } from "../../features/redux/store";
+import { toggleLessonDetails } from "../../features/redux/slices/lessonsSlice";
 
 const LessonCard = ({ id, title, duration, isCompleted }: ILesson) => {
+  const dispatch = useDispatch<AppDispatch>();
+  const isExpended = useSelector(
+    (state: RootState) => state.lessons.expandedLessons[id] || false
+  );
+
+  const toggleLessonExpended = () => {
+    dispatch(toggleLessonDetails(id));
+  };
+
   return (
     <div
       className={`${styles.lessonCard} ${isCompleted ? styles.completed : ""}`}
     >
       <h3 className={styles.lessonTitle}>{title}</h3>
-      <div className={styles.lessonDuration}>
-        Длительность урока: {duration}
+      <span
+        className={styles.showLessonCardDetails}
+        onClick={toggleLessonExpended}
+      >
+        {isExpended ? "Свернуть" : "Подробнее"}
+      </span>
+      <div className={styles.lessonCardDetails}>
+        {isExpended && (
+          <div className={styles.lessonDuration}>
+            Длительность урока: {duration}
+          </div>
+        )}
       </div>
     </div>
   );
--- src/features/redux/slices/lessonsSlice.ts
+++ src/features/redux/slices/lessonsSlice.ts
@@ -3,22 +3,25 @@ import type ILesson from "../../../types/lesson";
 
 interface ILessonsState {
   lessons: ILesson[];
+  expandedLessons: { [lessonId: string]: boolean };
 }
 
 const initialState: ILessonsState = {
   lessons: [],
+  expandedLessons: {},
 };
 
 const lessonsSlice = createSlice({
   name: "lesson",
   initialState,
   reducers: {
-    getLessons(state, action: PayloadAction<ILesson[]>) {
-      state.lessons = action.payload;
+    toggleLessonDetails(state, action: PayloadAction<string>) {
+      const lessonId = action.payload;
+      state.expandedLessons[lessonId] = !state.expandedLessons[lessonId];
     },
   },
 });
 
-export const { getLessons } = lessonsSlice.actions;
+export const { toggleLessonDetails } = lessonsSlice.actions;
 
 export default lessonsSlice.reducer;
