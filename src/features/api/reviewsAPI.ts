9586a4d1b1fd72fc5fef26722568c0af6f8cef6d
Desair1 <ilya_velichko_1995@mail.ru>
2025-06-03 18:30:08 +0300

    style: removed destructuringin reviewsAPI hook

--- src/features/api/reviewsAPI.ts
+++ src/features/api/reviewsAPI.ts
@@ -2,16 +2,12 @@ import { SERVER_URL } from "../../shared/constants/api";
 import type IReview from "../../types/review";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-interface GetReviewsForCoursesParams {
-  courseId: string;
-}
-
 export const reviewsAPI = createApi({
   reducerPath: "reviewsAPI",
   baseQuery: fetchBaseQuery({ baseUrl: SERVER_URL }),
   endpoints: (builder) => ({
-    getReviewsForCourse: builder.query<IReview[], GetReviewsForCoursesParams>({
-      query: ({ courseId }) => `/courses/${courseId}/reviews`,
+    getReviewsForCourse: builder.query<IReview[], string>({
+      query: (courseId) => `/courses/${courseId}/reviews`,
     }),
   }),
 });
